7eb3d26d99a54d06a9bab5276741063d71a8b624
Eli Joe <elijoe365@pm.me>
2020-05-31 17:45:19 +0100

    updated the montecarlo example with a more elegant list comprehension

--- ML/montecarlo.py
+++ ML/montecarlo.py
@@ -5,12 +5,6 @@ num = 1000000
 
 coords = [(random.uniform(-1, 1), random.uniform(-1, 1)) for i in range(num)]
 
-count = 0
-
-for item in coords:
-    if math.sqrt(item[0] ** 2 + item[1] ** 2) <= 1:
-        count += 1
-    else:
-        pass
+count = sum([math.sqrt(item[0] ** 2 + item[1] ** 2) <= 1 for item in coords])
 
 print(count/num * 4)
